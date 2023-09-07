dbd533d79acda858ede234531b54a4da6e41aec4
Rafael Jáber <rafael.jaber@hotmail.com>
2023-09-07 10:15:51 -0300

    corrigida a cagada

--- run-prod.sh
+++ run-prod.sh
@@ -2,4 +2,7 @@
 sudo docker run -e "SPRING_PROFILES_ACTIVE=prod" -p8080:8080 purchase-control/api
 
 
-docker container run --name estoque_api --hostname estoqueapi.giganet.psi.br -e "SPRING_PROFILES_ACTIVE=prod" --network vlan1810 --ip 172.26.10.254 -v /etc/localtime:/etc/localtime:ro -v /etc/timezone:/etc/timezone:ro -d --restart always --cpus 4 --memory 1024mb rafaeljaber/docker-estoque:289e1c551745 && \
+docker container run --name estoque_api --hostname estoqueapi.giganet.psi.br -e "SPRING_PROFILES_ACTIVE=prod" --network vlan1810 --ip 172.26.10.254 -v /etc/localtime:/etc/localtime:ro -v /etc/timezone:/etc/timezone:ro -d --restart always --cpus 1 --memory 1024mb rafaeljaber/estoque-sem-arm:7a0b51545cc5
+
+
+docker pull rafaeljaber/estoque-sem-arm:7a0b51545cc5
